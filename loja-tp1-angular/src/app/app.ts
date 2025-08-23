7870efd4a04fd538bb906aeee46d82f3762e8eb2
jose-aluno <josealuno70@gmail.com>
2025-08-23 00:46:37 +0000

    feat: estiliza footer

--- loja-tp1-angular/src/app/app.ts
+++ loja-tp1-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from './core/header/header';
+import { Footer } from './core/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
