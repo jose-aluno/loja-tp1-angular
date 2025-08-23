83a087a312081efdbb2a47531466c8ad4f214b00
jose-aluno <josealuno70@gmail.com>
2025-08-23 01:01:42 +0000

    feat: estiliza banner

--- loja-tp1-angular/src/app/app.ts
+++ loja-tp1-angular/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './core/header/header';
 import { Footer } from './core/footer/footer';
+import { Banner } from './core/banner/banner';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, Banner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
