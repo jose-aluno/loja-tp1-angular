b76f3bd87ce858195dddd778977fd095eddf7a0c
jose-aluno <josealuno70@gmail.com>
2025-08-29 23:34:38 +0000

    feat: contador component

--- loja-tp1-angular/src/app/app.ts
+++ loja-tp1-angular/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './core/header/header';
 import { Footer } from './core/footer/footer';
 import { Banner } from './core/banner/banner';
+import { QuantidadeControle } from './shared/quantidade-controle/quantidade-controle';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Banner],
+  imports: [Header, Footer, Banner, QuantidadeControle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
